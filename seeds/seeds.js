0bcd28f33aec8bd956d0f5a97e45c89a023084cc
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-06 01:19:12 -0400

    Update seeds.js

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -70,4 +70,68 @@ let workoutSeed = [
           }
         ]
     },
+    {
+        day: new Date().setDate(new Date().getDate()-5),
+        exercises: [
+          {
+            type: "resistance",
+            name: "Bench Press",
+            duration: 20,
+            weight: 300,
+            reps: 10,
+            sets: 4
+          }
+        ]
+      },
+      {
+        day: new Date(new Date().setDate(new Date().getDate() - 4)),
+        exercises: [
+          {
+            type: "resistance",
+            name: "Quad Press",
+            duration: 30,
+            weight: 300,
+            reps: 10,
+            sets: 4
+          }
+        ]
+      },
+      {
+        day: new Date(new Date().setDate(new Date().getDate() - 3)),
+        exercises: [
+          {
+            type: "resistance",
+            name: "Bench Press",
+            duration: 20,
+            weight: 300,
+            reps: 10,
+            sets: 4
+          }
+        ]
+      },
+      {
+        day: new Date(new Date().setDate(new Date().getDate() - 2)),
+        exercises: [
+          {
+            type: "resistance",
+            name: "Military Press",
+            duration: 20,
+            weight: 300,
+            reps: 10,
+            sets: 4
+          }
+        ]
+      }
 ];
+
+
+db.Workout.deleteMany({})
+  .then(() => db.Workout.collection.insertMany(workoutSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
