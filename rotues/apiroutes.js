a2ce9561148b9d140c3bcd9b2c56fd05590d8190
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-05 23:11:38 -0400

    Update apiroutes.js
    This api route will create a new workout

--- rotues/apiroutes.js
+++ rotues/apiroutes.js
@@ -12,8 +12,16 @@ module.exports = (app) => {
                 res.status(400).json(err)
             });
     });
-
-
+    // This api route will create a new workout
+    app.post('/ap/workouts', (req, res) => {
+        db.create({})
+        .then(dbWorkOutData => {
+        res.json(dbWorkOutData);
+        })
+        .catch(err => {
+        res.status(400).json(err);
+        });
+    })
 
     
 }
