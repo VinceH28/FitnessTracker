916051896a027704f9078a0df6020ec6275f64d5
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-10 19:55:30 -0400

    Update apiroutes.js

--- rotues/apiroutes.js
+++ rotues/apiroutes.js
@@ -3,8 +3,8 @@ const db = require('../models');
 
 module.exports = (app) => {
     // This api route will locate the workouts
-    app.get('/api/worksouts', (req, res) => {
-        db.find({})
+    app.get('/api/workouts', (req, res) => {
+        db.Workout.find({})
             .then(dbWorkOutData => {
                 res.json(dbWorkOutData);
             })
@@ -13,8 +13,8 @@ module.exports = (app) => {
             });
     });
     // This api route will create a new workout
-    app.post('/ap/workouts', (req, res) => {
-        db.create({})
+    app.post('/api/workouts', (req, res) => {
+        db.Workout.create({})
         .then(dbWorkOutData => {
         res.json(dbWorkOutData);
         })
@@ -25,11 +25,11 @@ module.exports = (app) => {
 
     //This will locate the most recent 10 workouts
     app.get('/api/workouts/range', (req, res) => {
-        db.dbWorkOutData.find({})
+        db.Workout.find({})
         .sort({day: -1})
         .limit(10)
         .then((dbWorkOutData) => {
-            re.json(dbWorkOutData);
+            res.json(dbWorkOutData);
         })
         .catch((err) => {
             res.json(err);
@@ -37,8 +37,8 @@ module.exports = (app) => {
     });
 
     // This will put a new workout on the sashboard using its ID
-    app.put('/api/worouts/:is', (req, res) => {
-        db.dbWorkOutData.findByIdAndUpdate(req.params.id, {
+    app.put('/api/workouts/:id', (req, res) => {
+        db.Workout.findByIdAndUpdate(req.params.id, {
             $push: {exercises: req.body},
         })
         .then((data) => {
